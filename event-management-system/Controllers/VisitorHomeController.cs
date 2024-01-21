d0692d2a87c0e11303652ed6774fe8c76629423c
hanzfernando <fernandohanz23@gmail.com>
2024-01-22 03:15:35 +0800

    scuffed login to org

--- event-management-system/Controllers/VisitorHomeController.cs
+++ event-management-system/Controllers/VisitorHomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json;
 
 namespace event_management_system.Controllers
 {
@@ -8,5 +10,37 @@ namespace event_management_system.Controllers
         {
             return View();
         }
+
+        public IActionResult Login() 
+        {
+
+            
+            return View();
+        }
+        
+        public IActionResult ValidateLogin([FromBody] UserCredential userCredential) 
+        {
+
+            Debug.WriteLine(JsonSerializer.Serialize(userCredential));
+
+            string email = userCredential.Email!;
+            string password = userCredential.Password!;
+
+            Debug.WriteLine(email);
+
+            if (email == "org")
+            {
+                Debug.WriteLine("inside");
+                return Json(new { redirectTo = Url.Action("Index", "OrganizationHome") });
+            }
+
+            return Ok();
+        }
+    }
+
+    public class UserCredential
+    {
+        public string? Email { get; set; }
+        public string? Password { get; set; }
     }
 }
---
+++ event-management-system/wwwroot/js/login.js
@@ -0,0 +1,42 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    setupLogin();
+})
+
+function setupLogin() {
+    var loginBtn = document.getElementById('loginBtn');
+    loginBtn.addEventListener('click', function () {
+        var email = document.getElementById('email').value;
+        var password = document.getElementById('password').value;
+
+        userCredential = {
+            Email: email,
+            Password: password,
+        }
+
+        console.log(userCredential)
+        sendLoginData(userCredential);
+    })
+}
+
+function sendLoginData(data) {
+    fetch('VisitorHome/ValidateLogin', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.redirectTo) {
+                window.location.href = data.redirectTo;
+            } else {
+                // Handle other responses
+                console.log("Login successful");
+            }
+        })
+        .catch(error => {
+            // Handle network or other errors
+            console.error('Error:', error);
+        });
+}
