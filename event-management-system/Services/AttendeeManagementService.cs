5dfaea12dc6ed503a1186548e558b066b5513e9f
denver0619 <jdrlaurian@gmail.com>
2024-01-24 14:48:06 +0800

    created attendeeManagementService

--- event-management-system/Services/AttendeeManagementService.cs
+++ event-management-system/Services/AttendeeManagementService.cs
@@ -5,7 +5,7 @@ using event_management_system.Domain.Repositories;
 
 namespace event_management_system.Services
 {
-    public class AttendeeManagementService
+    public class AttendeeManagementService : IDisposable    
     {
         private EventAttendeeRepository eventAttendeeRepository;
         private TicketRepository ticketRepository;
@@ -28,9 +28,49 @@ namespace event_management_system.Services
             {
                 EventAttendeeDataTransferObject eventAttendeeDataTransferObject = new EventAttendeeDataTransferObject(data);
                 eventAttendeeDataTransferObject.Student = studentRepository.GetByID(data.StudentID!);
-                attendeeList
+                attendeeList.Add(eventAttendeeDataTransferObject);
             }
-
+            Model.AttendeeList = attendeeList;
+            return Model;
         }
+
+        public void UpdateStudentApproveStatus(IEventAttendee eventAttendee)
+        {
+            eventAttendeeRepository.UpdateEventAttendee(eventAttendee);
+        }
+
+        public void GenerateTickets(string eventID)
+        {
+            List<IEventAttendee> attendees = eventAttendeeRepository.GetByEventID(eventID);
+            List<ITicket> ticketList = new List<ITicket>(); 
+            foreach(IEventAttendee attendee in attendees)
+            {
+                if (attendee.IsApproved)
+                {
+                    ITicket existingTickets = ticketRepository.GetByStudentIDandEventID(attendee.StudentID!, eventID);
+                    if(existingTickets.StudentID != null && existingTickets.StudentID == attendee.StudentID)
+                    {
+                        Ticket ticket = new Ticket()
+                        {
+                            EventID = eventID,
+                            StudentID = attendee.StudentID,
+                        };
+                        ticketList.Add(ticket);
+                    }
+                }
+            }
+            foreach (ITicket ticket in ticketList)
+            {
+                ticketRepository.AddTicket(ticket);
+            }
+        }
+
+        public void Dispose()
+        {
+            ticketRepository.Dispose();
+            studentRepository.Dispose();
+            eventAttendeeRepository.Dispose();
+        }
+
     }
 }
